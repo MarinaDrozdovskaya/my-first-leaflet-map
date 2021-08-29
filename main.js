dfe5b5d542eb894d9007d4e25023efa202c99871
Marina Drozdovskaya <85951373+MarinaDrozdovskaya@users.noreply.github.com>
2021-08-29 15:16:27 +0300

    .

--- main.js
+++ main.js
@@ -16,67 +16,7 @@ Esri_WorldImagery.addTo(map);
 
 // add Sand sample data with markers and popups
 
-let sandSamples = {
-	"type": "FeatureCollection",
-	"features": [
-		{
-			"type": "Feature",
-			"properties": {
-				"type": "sample",
-				"date": "2021-04-13"
-			},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					55.722291469573978,
-					-4.344250280136306
-				]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {
-				"type": "sample",
-				"date": "2021-04-13"
-			},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					55.72958707809448,
-					-4.349909506900781
-				]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {
-				"type": "sample",
-				"date": "2021-04-22"
-			},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					55.446356534957889,
-					-4.677193291718538
-				]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {
-				"type": "sample",
-				"date": "2021-04-21"
-			},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					55.251754224300388,
-					-4.492036685566651
-				]
-			}
-		}
-	]
-}
+
 
 let sandIcon = L.icon({
     iconUrl: 'sample.png',
@@ -96,125 +36,7 @@ sandSamplesLayer.bindPopup(function (layer) {
 }).addTo(map);
 
 
-// add Dive dite data with markers and popups
-
-let diveSites = {
-	"type": "FeatureCollection",
-	"features": [
-		{
-			"type": "Feature",
-			"properties": {
-				"type": "dive",
-				"date": "2021-04-15"
-			},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					55.487823486328128,
-					-4.276571639716837
-				]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {
-				"type": "dive",
-				"date": "2021-04-14"
-			},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					55.377960205078128,
-					-4.314915763836039
-				]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {
-				"type": "dive",
-				"date": "2021-04-16"
-			},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					55.49468994140625,
-					-4.349149953414805
-				]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {
-				"type": "dive",
-				"date": "2021-04-16"
-			},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					55.614166259765628,
-					-4.432674823020449
-				]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {
-				"type": "dive",
-				"date": "2021-04-17"
-			},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					55.69793701171875,
-					-4.509345086962858
-				]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {
-				"type": "dive",
-				"date": "2021-04-18"
-			},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					55.754241943359378,
-					-4.561366738166861
-				]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {
-				"type": "dive",
-				"date": "2021-04-19"
-			},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					55.677337646484378,
-					-4.633916949794763
-				]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {
-				"type": "dive",
-				"date": "2021-04-22"
-			},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					55.358734130859378,
-					-4.744781151396054
-				]
-			}
-		}
-	]
-}
+// add Dive site data with markers and popups
 
 let diveIcon = L.icon({
     iconUrl: 'dive.png',
@@ -249,6 +71,7 @@ let dataLayers = {
 
 L.control.layers(baseMaps, dataLayers, {
 	collapsed: false,
+	position: 'topleft',
 }).addTo(map);
 
 L.control.scale().addTo(map);
